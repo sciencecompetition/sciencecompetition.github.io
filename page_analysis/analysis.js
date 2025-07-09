56832d4a5b4cfd0147a2605554cf6656d7b6bece
Caspar9872 <s180401caspar@gmail.com>
2025-07-10 00:12:59 +0800

    debug the annoyiing secret vars

--- page_analysis/analysis.js
+++ page_analysis/analysis.js
@@ -11,4 +11,6 @@ if (day < 10) {
 }
 const full_date = `${year}${month}${day}`
 let today_waste;
-console.log(getData(`/Chilli/20250626}`))
+getData(`/Chilli/20250626}`).then((data) => {
+    console.log(data);
+})
