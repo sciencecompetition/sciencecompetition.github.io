4ffcc63e689daf0c4fdc692e56114ab89e2dec16
Caspar9872 <s180401caspar@gmail.com>
2025-07-10 00:20:26 +0800

    just debugging

--- page_analysis/analysis.js
+++ page_analysis/analysis.js
@@ -1,6 +1,7 @@
 const ele_today_waste = document.querySelector(".strong1");
 const raw_date = new Date();
 const year = raw_date.getFullYear();
+const food_types = ["Chilli","Corn","Meatball"]
 let month = raw_date.getMonth()+1;
 if (month < 10) {
     month = `0${month}`
@@ -10,7 +11,10 @@ if (day < 10) {
     day = `0${day}`
 }
 const full_date = `${year}${month}${day}`
-let today_waste;
-getData(`/Chilli/20250626}`).then((data) => {
-    console.log(data);
+let wasteObject;
+food_types.forEach((item) => {
+    getData(`/${item}/20250626}`).then((data) => {
+        wasteObject[item]=data;
+    })
 })
+console.log(wasteObject)
