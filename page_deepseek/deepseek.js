29cbb2aea2670892a78d42f167a5cffb490df97c
Caspar9872 <s180401caspar@gmail.com>
2025-07-22 01:23:45 +0800

    this shall work, debug 6th time alr

--- page_deepseek/deepseek.js
+++ page_deepseek/deepseek.js
@@ -20,7 +20,7 @@ const chatbox_parent = document.querySelector(".chatbox_parent");
 const home_btn = document.getElementById("homeBtn");
 const checkbox = document.getElementById("include_data_checkbox");
 const deepseek_url = "https://router.huggingface.co/fireworks-ai/inference/v1/chat/completions";
-const authtoken = ""
+const authtoken = window.deepseek_API_key;
 const raw_date = new Date("2025-6-27");
 const month_before_raw = new Date(raw_date);
 month_before_raw.setDate(raw_date.getDate() - 29);
